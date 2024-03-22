b147ccb660a2b9ed86c8d57ef62045ebc1ed6eea
ghersmann <gustav@gustavhersmann.com>
2024-03-22 15:48:56 +0100

    Coding Task 6: List Rendering

---
+++ 06-list-rendering/script.js
@@ -0,0 +1,48 @@
+Vue.createApp({
+  data() {
+    return {
+      books: [
+        {
+          title: "Practical Rust Web Projects",
+          isbn: "9781484265888",
+          author: "Shing Lyu",
+          publisher: "Apress",
+          price: "$28.75",
+          numPages: 256,
+        },
+        {
+          title: "Using WebPagetest",
+          isbn: "9781491902592",
+          author: "Rick Viscomi, Andy Davies, Marcel Duran",
+          publisher: "O'Reilly Media",
+          price: "$25.80",
+          numPages: 214,
+        },
+        {
+          title: "Web Scraping with Python",
+          isbn: "9781491910290",
+          author: "Ryan Mitchell",
+          publisher: "O'Reilly Media",
+          price: "$14.00",
+          numPages: 256,
+        },
+        {
+          title: "High Performance Mobile Web",
+          isbn: "9781491912553",
+          author: "Maximiliano Firtman",
+          publisher: "O'Reilly Media",
+          price: "$7.00",
+          numPages: 326,
+        },
+        {
+          title: "Joe's Garage",
+          isbn: "9781491423553",
+          author: "Frank Zappa",
+          publisher: "Mothers of Invention",
+          price: "$237.00",
+          numPages: 1326,
+        },
+      ],
+    };
+  },
+}).mount("#app");
